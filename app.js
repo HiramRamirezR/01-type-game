9255e0e91bb436a12ad8245875c118694de36d3e
hiramramirez <hiram.j.ramirez@gmail.com>
2022-10-03 22:09:22 -0500

    The timer works, the level and the points. It just need to make the new level with the new keys to play.

--- app.js
+++ app.js
@@ -1,24 +1,64 @@
 const abecedario = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z']
-let letras = abecedario.sort(() => Math.random() - Math.random()).slice(0, 5)
-const mensaje = document.querySelector('.mensaje')
-let puntosTexto = document.querySelector('.puntos-texto')
-let puntos = 0
 
-const teclasDiv = document.querySelector('.teclas-div')
-for (let i = 0; i < letras.length; i++) {
-  let newSpan = document.createElement('span')
-  teclasDiv.appendChild(newSpan)
-  newSpan.innerText = letras[i]
-  newSpan.classList.add(`${letras[i]}`)
+let puntos = 0
+let puntosNivel = 0
+let nivel = 1
+
+
+
+let puntosTexto = document.querySelector('.puntos-texto')
+let nivelTexto = document.querySelector('.nivel-texto')
+nivelTexto.innerText = nivel
+
+const crearTeclas = () => {
+  let letras = abecedario.sort(() => Math.random() - Math.random()).slice(0, nivel)
+  const teclasDiv = document.querySelector('.teclas-div')
+  for (let i = 0; i < letras.length; i++) {
+    let newSpan = document.createElement('span')
+    teclasDiv.appendChild(newSpan)
+    newSpan.innerText = letras[i]
+    newSpan.classList.add(`${letras[i]}`)
+  }
 }
 
-window.addEventListener('keydown', (e) => {
-  const key = document.querySelector(`span[class="${e.key}"]`)
-  if (!key) {
-    mensaje.innerText = 'Ups! Sigue practicando'
-    return
+const detectarTeclas = () => {
+  window.addEventListener('keydown', (e) => {
+    const key = document.querySelector(`span[class="${e.key}"]`)
+    if (!key) {
+      document.querySelector('.container').classList.add('hide')
+    }
+    puntos++
+    puntosTexto.innerText = puntos
+    key.remove()
+    puntosNivel++
+    if (puntosNivel == nivel) {
+      nivel++
+      nivelTexto.innerText = nivel
+    }
+  })
+
+  //Contador
+
+  let timeLeft = 60;
+  let timeInitial = 0
+  const downloadTimer = setInterval(function(){
+    if(timeLeft <= 0){
+      clearInterval(downloadTimer);
+      document.querySelector('.container').classList.add('hide')
+    }
+    document.querySelector('.tiempo-texto').innerText = 60 - timeInitial;
+    document.getElementById("progressBar").value = 60 - timeInitial;
+    timeInitial++;
+    timeLeft--
+  }, 1000);
+}
+
+const juego = () => {
+  console.log(nivel)
+  if (nivel <= 10) {
+    crearTeclas()
+    detectarTeclas()
   }
-  puntos++
-  puntosTexto.innerText = puntos
-  key.remove()
-})
+}
+
+juego()
